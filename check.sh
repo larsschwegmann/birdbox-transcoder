a6c2c9f5fb2d336ddbf72a1d55d98c7ac68cf384
Lars Schwegmann <schwegmannlars@gmail.com>
2023-05-26 17:15:03 +0200

    update cli params

--- check.sh
+++ check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+TWITCH_CLIENT_ID=$1
+TWITCH_CLIENT_SECRET=$2
+TWITCH_USER_ID=$3
+
 TWITCH_TOKEN=$(curl -s -X POST -d "client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials" "https://id.twitch.tv/oauth2/token" | jq -r '.access_token' )
 
 is_empty=$(curl -s -H "Authorization: Bearer ${TWITCH_TOKEN}" -H "Client-ID: ${TWITCH_CLIENT_ID}" "https://api.twitch.tv/helix/streams?user_id=${TWITCH_USER_ID}" | jq -r '.data | length == 0')
