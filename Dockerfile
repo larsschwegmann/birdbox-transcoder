f37523fc774aa0b14ec40778f2d6002bead14651
Lars Schwegmann <schwegmannlars@gmail.com>
2023-05-26 15:41:14 +0200

    add check

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM linuxserver/ffmpeg:latest
 
-RUN apt-get update && apt-get install -y fonts-dejavu
+RUN apt-get update && apt-get install -y fonts-dejavu jq
 
 COPY execute.sh .
+COPY check.sh .
 ENTRYPOINT ["/usr/bin/env"]
 CMD [ "/bin/bash", "execute.sh" ]
---
+++ check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TWITCH_TOKEN=$(curl -s -X POST -d "client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials" "https://id.twitch.tv/oauth2/token" | jq -r '.access_token' )
+
+is_empty=$(curl -s -H "Authorization: Bearer ${TWITCH_TOKEN}" -H "Client-ID: ${TWITCH_CLIENT_ID}" "https://api.twitch.tv/helix/streams?user_id=${TWITCH_USER_ID}" | jq -r '.data | length == 0')
+
+if [[ $is_empty == "true" ]]; then
+  echo "Offline"
+  exit 1
+else
+  echo "Live"
+  exit 0
+fi
